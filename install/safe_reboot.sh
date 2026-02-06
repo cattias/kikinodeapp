ba33ef463d6e2203839032d268364e261db8a601
Christophe Attias <christophe.attias@gmail.com>
2026-02-06 15:40:48 +0100

    safe reboot

--- install/safe_reboot.sh
+++ install/safe_reboot.sh
@@ -10,7 +10,7 @@ echo "🧹 Draining kikiflix-ubuntu ..."
 # --disable-eviction is the key to bypassing those Paperless/Immich errors
 kubectl drain kikiflix-ubuntu --ignore-daemonsets --delete-emptydir-data --force --grace-period=30 --disable-eviction
 # for stuck in Terminating :
-kubectl get pods -A | grep Terminating | awk '{print "kubectl delete pod " $2 " -n " $1 " --force --grace-period=0"}' | sh
+# kubectl get pods -A | grep Terminating | awk '{print "kubectl delete pod " $2 " -n " $1 " --force --grace-period=0"}' | sh
 
 # 4. Stop K0s engine
 echo "🛑 Stopping k0s..."
@@ -19,8 +19,3 @@ sudo k0s stop
 echo "♻️ Rebooting now..."
 sync
 sudo reboot
-
-# after reboot
-kubectl uncordon kikiflix-ubuntu
-kubectl -n argocd scale statefulset/argocd-application-controller --replicas=1
-sudo systemctl restart tailscaled
